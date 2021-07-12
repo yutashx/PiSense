30cfa15fd9109c158b93de1ffa13069836f876e0
yuashx <yutashx@gmail.com>
2021-07-12 23:42:14 +0900

    initial commit

---
+++ client/EtherSenseClient.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python
+import sys, getopt
+import asyncore
+import numpy as np
+import pickle
+import socket
+import struct
+import cv2
+import open3d as o3d
+
+
+print("Number of arguments:", len(sys.argv), "arguments.")
+print("Argument List:", str(sys.argv))
+pcd_save_path = "./savedata/"
+mc_ip_address = "224.0.0.1"
+#local_ip_address = "192.168.0.1"
+local_ip_address = "localhost"
+port = 1024
+chunk_size = 4096
+
+
+def main(argv):
+    multi_cast_message(mc_ip_address, port, "EtherSensePing")
+
+
+#UDP client for each camera server
+class ImageClient(asyncore.dispatcher):
+    def __init__(self, server, source):
+        asyncore.dispatcher.__init__(self, server)
+        self.address = server.getsockname()[0]
+        self.port = source[1]
+        self.buffer = bytearray()
+        self.windowName = self.port
+        # open cv window which is unique to the port 
+        cv2.namedWindow("window"+str(self.windowName))
+        #self.vis = o3d.visualization.Visualizer()
+        self.remainingBytes = 0
+        self.frame_id = 0
+
+    def handle_read(self):
+        if self.remainingBytes == 0:
+            # get the expected frame size
+            self.frame_length = struct.unpack("<I", self.recv(4))[0]
+            # get the timestamp of the current frame
+            self.timestamp = struct.unpack("<d", self.recv(8))
+            self.remainingBytes = self.frame_length
+
+        # request the frame data until the frame is completely in buffer
+        data = self.recv(self.remainingBytes)
+        self.buffer += data
+        self.remainingBytes -= len(data)
+        if len(self.buffer) == self.frame_length:
+            self.handle_frame()
+
+    def handle_frame(self):
+        # convert the frame from string to numerical data
+        imdata = pickle.loads(self.buffer)
+        timestamp = str(self.timestamp[0]).replace(".", "_")
+        color_data, depth_data, intr = imdata[0], imdata[1], list(imdata[2])
+
+        color_image = o3d.geometry.Image(cv2.cvtColor(color_data, cv2.COLOR_BGR2RGB))
+        depth_image = o3d.geometry.Image(depth_data)
+        rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(color_image, depth_image, convert_rgb_to_intensity=False, depth_scale=200, depth_trunc=200)
+        intr = [int(intr[0]), int(intr[1]), *intr[2:]]
+        pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd, o3d.camera.PinholeCameraIntrinsic(*intr))
+        rotation = np.array([[1,0,0], [0,-1,0], [0,0,-1]])
+        pcd.rotate(rotation)
+        o3d.io.write_point_cloud(pcd_save_path + f"{timestamp}.pcd", pcd)
+        print(f"save {timestamp}.pcd")
+
+        #depth_doubled = cv2.resize(depth_data, (0,0), fx=2, fy=2, interpolation=cv2.INTER_NEAREST) # depth image scale is doubled
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_data, alpha=0.03), cv2.COLORMAP_JET)
+        cv2.putText(color_data, timestamp, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (65536), 2, cv2.LINE_AA)
+        images = np.concatenate((color_data, depth_colormap), axis=1)
+        cv2.imshow("window"+str(self.windowName), images)
+        cv2.waitKey(1)
+
+        """
+        cv2.imwrite(pcd_save_path + f"color_{timestamp}.png", color_data)
+        cv2.imwrite(pcd_save_path + f"depth_{timestamp}.png", depth_data)
+        self.vis.clear_geometries()
+        self.vis.add_geometry(pcd)
+        self.vis.poll_events()
+        self.vis.update_renderer()
+        """
+
+        self.buffer = bytearray()
+        self.frame_id += 1
+
+    def readable(self):
+        return True
+
+
+class EtherSenseClient(asyncore.dispatcher):
+    def __init__(self):
+        asyncore.dispatcher.__init__(self)
+        self.server_address = ("", 1024)
+        # create a socket for TCP connection between the client and server
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.settimeout(5)
+
+        self.bind(self.server_address)
+        self.listen(10)
+
+    def writable(self): 
+        return False # don"t want write notifies
+
+    def readable(self):
+        return True
+
+    def handle_connect(self):
+        print("connection recvied")
+
+    def handle_accept(self):
+        pair = self.accept()
+        #print(self.recv(10))
+        if pair is not None:
+            sock, addr = pair
+            print ("Incoming connection from %s" % repr(addr))
+            # when a connection is attempted, delegate image receival to the ImageClient 
+            handler = ImageClient(sock, addr)
+
+def multi_cast_message(ip_address, port, message):
+    # send the multicast message
+    multicast_group = (ip_address, port)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    connections = {}
+    try:
+        # Send data to the multicast group
+        print(f"sending {message} {str(multicast_group)}")
+        sent = sock.sendto(message.encode(), multicast_group)
+
+        # defer waiting for a response using Asyncore
+        client = EtherSenseClient()
+        asyncore.loop()
+
+        # Look for responses from all recipients
+
+    except socket.timeout:
+        print("timed out, no more responses")
+    finally:
+        print(sys.stderr, "closing socket")
+        sock.close()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ server/EtherSenseServer.py
@@ -0,0 +1,162 @@
+#!/usr/bin/python
+import pyrealsense2 as rs
+import sys, getopt
+import asyncore
+import numpy as np
+import pickle
+import socket
+import struct
+
+
+print('Number of arguments:', len(sys.argv), 'arguments.')
+print('Argument List:', str(sys.argv))
+#mc_ip_address = '224.0.0.1'
+mc_ip_address = 'localhost'
+port = 1024
+chunk_size = 4096
+clipping_distance_in_meters = 2.0
+camera_width = 640
+camera_height = 360
+#rs.log_to_console(rs.log_severity.debug)
+
+
+def getColorDepthTimestamp(pipeline, color_depth_filter, align, depth_sensor):
+    frames = pipeline.wait_for_frames()
+    # take owner ship of the frame for further processing
+    #frames.keep()
+    aligned_frames = align.process(frames)
+    color = aligned_frames.get_color_frame()
+    depth = aligned_frames.get_depth_frame()
+
+    depth_scale = depth_sensor.get_depth_scale()
+    clipping_distance = clipping_distance_in_meters / depth_scale
+
+    if color and depth:
+        # represent the frame as a numpy array
+        colorData = color.get_data()
+        depthData = depth.get_data()
+        colorMat = np.asanyarray(colorData)
+        depthMat = np.asanyarray(depthData)
+        depthMat = np.where((depthMat > clipping_distance) | (depthMat <= 0), 0, depthMat)
+        ts = frames.get_timestamp()
+
+        return colorMat, depthMat, ts
+    else:
+        return None, None, None
+
+def openPipeline():
+    cfg = rs.config()
+    cfg.enable_stream(rs.stream.depth, camera_width, camera_height, rs.format.z16, 30)
+    cfg.enable_stream(rs.stream.color, camera_width, camera_height, rs.format.bgr8, 30)
+    pipeline = rs.pipeline()
+    pipeline_profile = pipeline.start(cfg)
+    align = rs.align(rs.stream.color)
+    sensor = pipeline_profile.get_device().first_depth_sensor()
+    intr = pipeline_profile.get_stream(rs.stream.color).as_video_stream_profile().get_intrinsics()
+
+    return pipeline, align, intr, sensor
+
+class DevNullHandler(asyncore.dispatcher_with_send):
+
+    def handle_read(self):
+        print(self.recv(1024))
+
+    def handle_close(self):
+        self.close()
+
+
+class EtherSenseServer(asyncore.dispatcher):
+    def __init__(self, address):
+        asyncore.dispatcher.__init__(self)
+        print("Launching Realsense Camera Server")
+        try:
+            self.pipeline, self.align, intr, self.depth_sensor = openPipeline()
+            self.intr = np.asanyarray([intr.width, intr.height, intr.fx, intr.fy, intr.ppx, intr.ppy])
+        except:
+            print("Unexpected error: ", sys.exc_info()[1])
+            sys.exit(1)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        print('sending acknowledgement to', address)
+
+        # reduce the resolution of the depth image using post processing
+        self.decimate_filter = rs.decimation_filter()
+        self.decimate_filter.set_option(rs.option.filter_magnitude, 2)
+        self.frame_data = ''
+        self.connect((address[0], 1024))
+        self.packet_id = 0
+
+
+    def handle_connect(self):
+        print("connection received")
+
+
+    def writable(self):
+        return True
+
+
+    def update_frame(self):
+        color, depth, timestamp = getColorDepthTimestamp(self.pipeline, self.decimate_filter, self.align, self.depth_sensor)
+        if (color is not None) and (depth is not None):
+            # convert the depth image to a string for broadcast
+            data = pickle.dumps([color, depth, self.intr])
+            #print(color,depth)
+            # capture the lenght of the data portion of the message
+            length = struct.pack('<I', len(data))
+            # include the current timestamp for the frame
+            ts = struct.pack('<d', timestamp)
+            # for the message for transmission
+            self.frame_data = length + ts + data
+
+
+    def handle_write(self):
+        # first time the handle_write is called
+        if not hasattr(self, 'frame_data'):
+            self.update_frame()
+        # the frame has been sent in it entirety so get the latest frame
+        if len(self.frame_data) == 0:
+            self.update_frame()
+        else:
+            # send the remainder of the frame_data until there is no data remaining for transmition
+            remaining_size = self.send(self.frame_data)
+            self.frame_data = self.frame_data[remaining_size:]
+
+
+    def handle_close(self):
+        self.close()
+
+
+class MulticastServer(asyncore.dispatcher):
+    def __init__(self, host = mc_ip_address, port=1024):
+        asyncore.dispatcher.__init__(self)
+        server_address = ('', port)
+        self.create_socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.bind(server_address)
+
+    def handle_read(self):
+        data, addr = self.socket.recvfrom(42)
+        print('Recived Multicast message %s bytes from %s' % (data, addr))
+        # Once the server recives the multicast signal, open the frame server
+        EtherSenseServer(addr)
+        print(sys.stderr, data)
+
+    def writable(self):
+        return False # don't want write notifies
+
+    def handle_close(self):
+        self.close()
+
+    def handle_accept(self):
+        channel, addr = self.accept()
+        print('received %s bytes from %s' % (data, addr))
+
+
+def main(argv):
+    # initalise the multicast receiver 
+    server = MulticastServer()
+    # hand over excicution flow to asyncore
+    asyncore.loop()
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+
